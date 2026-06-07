49b997549bb272a6a517954680ec137f3f0177f6
Fangkuai <2248716227@qq.com>
2026-06-07 22:51:38 +0800

    message 26 :add gunicorn to virtualenv requirements

--- notes/settings.py
+++ notes/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e@ml#)ach4!#wsuw5vlqp8cxizy1q4pnyj_p&bwaop%5$c0*=0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['123.57.141.186']
 
 
 # Application definition
